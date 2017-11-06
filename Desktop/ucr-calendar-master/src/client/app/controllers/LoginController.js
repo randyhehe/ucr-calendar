569c94ac3581b837c3c1f40dd5c567ccc8dabab1
kevvvnguyen <kevinknguyen24@gmail.com>
2017-11-06 13:53:24 -0800

    merged everything

--- Desktop/ucr-calendar-master/src/client/app/controllers/LoginController.js
+++ Desktop/ucr-calendar-master/src/client/app/controllers/LoginController.js
@@ -1,13 +1,24 @@
 angular.module('LoginController', ['ngCookies']).controller('LoginController', function($scope, $window, $cookies, UserService) {
     $scope.myFunction = function(user, password) {
         // have a check for user field and password, nonempty
-        
+
         UserService.authenticateUser(user, password).then(function(res) {
             $cookies.put('token', res.data.token);
             $window.location.href = '/calendar';
         }, function(err) {
             // show a message on the screen saying that there is an error.
+            document.getElementById('password_message').style.color = 'red';
+            document.getElementById('password_message').innerHTML = 'Incorrect password.';
             console.log(err);
         });
     }
+
+    $scope.passwordFunction = function() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
 });
--- Desktop/ucr-calendar-master/src/client/app/controllers/RegisterController.js
+++ Desktop/ucr-calendar-master/src/client/app/controllers/RegisterController.js
@@ -5,7 +5,7 @@ angular.module('RegisterController', []).controller('RegisterController', functi
             // userrname is empty, output error onto screen
             console.log("username is empty");
             numErrors++;
-        } 
+        }
 
         const usernameRegex = /^[a-zA-Z0-9]+$/;
         if (!usernameRegex.test(username)) {
@@ -16,32 +16,52 @@ angular.module('RegisterController', []).controller('RegisterController', functi
         if (email === undefined || email === '') {
             // email is empty, output error onto screen
             console.log('email is empty');
+            document.getElementById('email_message').style.color = 'red';
+            document.getElementById('email_message').innerHTML = 'Please enter your email.';
             numErrors++;
-        } 
+        }
         if (firstPassword === undefined || firstPassword === '') {
             // first password is empty, output error onto screen
             console.log("first password is empty");
+            document.getElementById('first_message').style.color = 'red';
+            document.getElementById('first_message').innerHTML = 'Password cannot be empty!';
             numErrors++;
         }
         if (secondPassword === undefined || secondPassword === '') {
             // second password is empty, output error onto screen
             console.log("second password is empty");
+            document.getElementById('second_message').style.color = 'red';
+            document.getElementById('second_message').innerHTML = 'Please verify your password.';
             numErrors++;
         }
         if (firstPassword !== secondPassword) {
             // passwords don't match, output error onto screen
             console.log("passwords don't match");
+            document.getElementById('second_message').style.color = 'red';
+            document.getElementById('second_message').innerHTML = 'Passwords do not match!';
             numErrors++;
         }
-        
+
         if (numErrors === 0) {
+            var node = document.getElementById('email_message');
+            while (node.hasChildNodes()) {
+                node.removeChild(node.firstChild);
+            }
+            var node = document.getElementById('first_message');
+            while (node.hasChildNodes()) {
+                node.removeChild(node.firstChild);
+            }
+            var node = document.getElementById('second_message');
+            while (node.hasChildNodes()) {
+                node.removeChild(node.firstChild);
+            }
             createUser(email, username, firstPassword);
         }
     }
 
     function createUser(email, username, password, callback) {
         email = email.toLowerCase();
-        
+
         UserService.userExists(username).then(function(exists) {
             if (exists) throw new Error("Username exists!");
             return UserService.userExists(email);
