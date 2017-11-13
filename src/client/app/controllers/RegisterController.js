88eec5770e7f715b6489a95442112fa5d388640f
= <rhe004@ucr.edu>
2017-11-12 23:18:35 -0800

    incorporated Kevin's changees and made a few modifications

--- src/client/app/controllers/RegisterController.js
+++ src/client/app/controllers/RegisterController.js
@@ -1,52 +1,74 @@
 angular.module('RegisterController', []).controller('RegisterController', function($scope, $window, UserService) {
     $scope.register = function(username, email, firstPassword, secondPassword) {
         let numErrors = 0;
-        if (username === undefined || username === '') {
-            // userrname is empty, output error onto screen
-            console.log("username is empty");
-            numErrors++;
-        } 
 
+        // Check Username
         const usernameRegex = /^[a-zA-Z0-9]+$/;
-        if (!usernameRegex.test(username)) {
-            console.log('Username can only have characters from a-z, A-Z, and 0-9');
+        if (username === undefined || username === '') {
+            $scope.showUsernameError = true;
+            $scope.usernameError = 'Username cannot be empty.';
             numErrors++;
+        } else if (!usernameRegex.test(username)) {
+            $scope.showUsernameError = true;
+            $scope.usernameError = 'Username can only have characters from a-z, A-Z, and 0-9';
+            numErrors++;
+        } else {
+            $scope.showUsernameError = false;
         }
 
+        // Check Email
         if (email === undefined || email === '') {
-            // email is empty, output error onto screen
-            console.log('email is empty');
+            $scope.emailError = 'Email cannot be empty.';
+            $scope.showEmailError = true;  
             numErrors++;
-        } 
+        } else {
+            $scope.showEmailError = false;
+        }
+
+        // Check Password
         if (firstPassword === undefined || firstPassword === '') {
-            // first password is empty, output error onto screen
-            console.log("first password is empty");
+            $scope.showFirstPasswordError = true;
             numErrors++;
+        } else {
+            $scope.showFirstPasswordError = false;
         }
         if (secondPassword === undefined || secondPassword === '') {
-            // second password is empty, output error onto screen
-            console.log("second password is empty");
+            $scope.secondPasswordError = 'Please verify your password.';
+            $scope.showSecondPasswordError = true;
             numErrors++;
+        } else if (firstPassword !== secondPassword) {
+            $scope.secondPasswordError = 'Passwords do not match.';
+            $scope.showSecondPasswordError = true;
+             numErrors++;
+        } else {
+            $scope.showSecondPasswordError = false;
         }
-        if (firstPassword !== secondPassword) {
-            // passwords don't match, output error onto screen
-            console.log("passwords don't match");
-            numErrors++;
-        }
-        
+
         if (numErrors === 0) {
             createUser(email, username, firstPassword);
         }
     }
 
+    $scope.redirectHome = function() {
+        $window.location.href = '/';        
+    }
+
     function createUser(email, username, password, callback) {
         email = email.toLowerCase();
-        
+
         UserService.userExists(username).then(function(exists) {
-            if (exists) throw new Error("Username exists!");
+            if (exists){
+                $scope.usernameError = 'Username is already in use.';
+                $scope.showUsernameError = true;                
+                throw new Error("Username exists!")
+            }
             return UserService.userExists(email);
         }).then(function(exists) {
-            if (exists) throw new Error("Email exists!");
+            if (exists){
+                $scope.emailError = 'Email is already in use.';
+                $scope.showEmailError = true;                
+                throw new Error("Email exists!");
+            }
             return UserService.createUser(email, username, password);
         }).then(function(res) {
             $window.location.href = '/';
